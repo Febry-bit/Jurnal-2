aca201b25cdb849d7d5bbbe98be18a27ae227a5a
Febrymp <71366528+Febry-bit@users.noreply.github.com>
2020-09-16 13:12:21 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+     public static void main(String[] args) {
+	     System.out.println("Nama : Febry Mahendra Pratama");
+		 System.out.println("Nim : 6706202015");
+	     System.out.println("Kelas : D3IF-44-02");
+	 }
+}	 
